60b44ff769c3e9f076d0769860868d526bb70112
Abdoulaye GAYE <layegaye001@gmail.com>
2024-01-03 20:57:32 +0000

    Seance du 3 janvier 2024

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module sn.dev.sponsorshipapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires lombok;
+
+    opens sn.dev.sponsorshipapp to javafx.fxml;
+    exports sn.dev.sponsorshipapp;
+    exports sn.dev.sponsorshipapp.controllers;
+    opens sn.dev.sponsorshipapp.controllers to javafx.fxml;
+}
